18fea866f9b0dd569e7a3064a42fe25419735aca
Samuel Javelle <samueljavelle@hotmail.com>
2024-05-06 14:58:20 +0200

    fix backend link

--- pages/space/[...id].js
+++ pages/space/[...id].js
@@ -3,7 +3,9 @@ import io from 'socket.io-client';
 
 
 function SpaceId() {
-    const socket = io('http://localhost:3000/');
+    // const socket = io('http://localhost:3000/');
+    const socket = io('https://gora-back.vercel.app/');
+
     return <Space socket={socket}/>;
 }
 
