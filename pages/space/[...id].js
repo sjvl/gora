61d5c741c4bf725093083df6168b3caeee0083e2
Samuel Javelle <samueljavelle@hotmail.com>
2024-05-06 16:07:45 +0200

    try koyeb

--- pages/space/[...id].js
+++ pages/space/[...id].js
@@ -4,7 +4,11 @@ import io from 'socket.io-client';
 
 function SpaceId() {
     // const socket = io('http://localhost:3000/');
-    const socket = io('https://gora-back.vercel.app/');
+    // const socket = io('https://gora-back.vercel.app/');
+    const socket = io('https://gora-gora-97ecbe0c.koyeb.app/');
+
+
+    
 
     return <Space socket={socket}/>;
 }
